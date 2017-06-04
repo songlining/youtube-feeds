d3101355eb8dad26e742b88c9489c5fbf8677315
Larry Song <songlining@gmail.com>
2017-06-04 22:34:13 +1000

    clean up

--- index.js
+++ index.js
@@ -29,9 +29,7 @@ app.set('port', process.env.PORT || 6003);
 // app.set('port', appEnv.port);
 
 // prepare server
-app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
 app.use('/js', express.static(__dirname + '/node_modules/clipboard/dist')); 
 
