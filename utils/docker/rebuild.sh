e46366b06ad61ca78436ec9c479922f0b2dc4bd2
Larry Song <songlining@gmail.com>
2017-06-27 09:51:36 +1000

    adding utils

---
+++ utils/docker/rebuild.sh
@@ -0,0 +1,9 @@
+echo "Removing the image youtube-feeds:latest..."
+docker rmi `docker images|grep youtube-feeds|grep latest|awk '{print $3}'`
+echo "Tagging the latest image as youtube-feeds:latest"
+docker tag $(docker build -q .|awk -F: '{print $2}') songlining/youtube-feeds:latest
+echo "Pushing youtube-feeds:latest to dockerhub"
+docker push songlining/youtube-feeds:latest
+cd k8s
+echo "Reload the image in K8S"
+./reset-all.sh
