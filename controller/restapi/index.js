50deff55166b7d0054e7d45a74c87f50b067503a
Lining Song <songlining@gmail.com>
2020-02-27 18:50:29 +1100

    minor changes

--- controller/restapi/index.js
+++ controller/restapi/index.js
@@ -87,12 +87,12 @@ exports.add_playlist = function(req, res) {
 // return the last n episodes of a playlist
 function list_episodes(playlist, n) {
     return new Promise(function(resolve, reject) {
-        ytpl(playlist, async function(err, pl) {
+        ytpl(playlist, {limit: n}, async function(err, pl) {
             if(err) {
                 reject(err);
             };
             let a = [];
-            let pl_latest = pl.items.reverse().slice(0, n);
+            let pl_latest = pl.items;
 	          for (let i = 0, len = pl_latest.length; i < len; i++) {
                 let id = pl_latest[i].id;
                 let title = pl_latest[i].title;
